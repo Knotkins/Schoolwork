7f21edaf5c279f494dbd8b80abe89ce2932b6196
NanitusJello <44480655+Nanitus@users.noreply.github.com>
2020-10-04 22:57:48 -0400

    Added linear motion and rotational motion to the project

---
+++ Physics/Physics/Assets/Scripts/RotationalMotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationalMotion : MonoBehaviour {
+    // Determines the angular rotation and the previous rotation of the cube
+    public float angularRotation;
+    public Quaternion prevRotation;
+
+    void Start() {
+        prevRotation = this.transform.rotation;
+    }
+    
+    void Update() {
+        // q += q * w * dt / 2
+        prevRotation = Quaternion.AngleAxis(angularRotation *(Time.deltaTime/2), Vector3.up);
+        this.transform.rotation *= prevRotation;
+    }
+}
