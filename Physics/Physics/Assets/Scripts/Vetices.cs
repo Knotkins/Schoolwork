f8f4ca8769053c89ebcac27747e50d83b6d7e6e7
NanitusJello <44480655+Nanitus@users.noreply.github.com>
2020-10-21 12:17:17 -0400

    Stuff

--- Physics/Physics/Assets/Scripts/Vetices.cs
+++ Physics/Physics/Assets/Scripts/Vetices.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vetices : MonoBehaviour{
+public class Vetices : MonoBehaviour
+{
     Mesh mesh;
     Vector3[] vertices;
-    public string ObjectName;
 
-    void Start() {
+    void Start()
+    {
         mesh = GetComponent<MeshFilter>().mesh;
         vertices = mesh.vertices;
+
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = transform.TransformPoint(vertices[i]);
+
+        }
+
     }
 
-    void Update() {
-        for (var i = 0; i < vertices.Length; i++) {
+    void Update()
+    {
+        for (var i = 0; i < vertices.Length; i++)
+        {
             Debug.Log(vertices[i]);
+
         }
+        
     }
 }
